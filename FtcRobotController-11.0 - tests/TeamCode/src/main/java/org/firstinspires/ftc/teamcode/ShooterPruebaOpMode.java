d7003caae6a0ff35c75d0b3823f086f1fc18a7cd
iria-mqr <iria@mqrobots.org>
2025-12-06 13:05:22 +0100

    Update ShooterPruebaOpMode.java
    Adding shooter controll

--- FtcRobotController-11.0 - tests/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterPruebaOpMode.java
+++ FtcRobotController-11.0 - tests/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterPruebaOpMode.java
@@ -4,28 +4,52 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp
 public class ShooterPruebaOpMode extends OpMode {
     private DcMotorEx shooter1;
     private DcMotorEx shooter2;
-    private double maxVel = 2600;
+    private final double maxVel = 2600;
+    private double velocity = 1600;
+    private int shooting = 0;
 
+    private long lastChangeTime = 0; // time of last velocity change (in ms)
+    private long cooldown = 500; // 0.5 seconds
     @Override
     public void init() {
         shooter1=hardwareMap.get(DcMotorEx.class,"shooter1");
         shooter2=hardwareMap.get(DcMotorEx.class,"shooter2");
         shooter1.setDirection(DcMotorSimple.Direction.REVERSE);
         shooter2.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter1.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        shooter2.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
     public void loop() {
-        shooter1.setVelocity(gamepad1.left_trigger*maxVel);
-        shooter2.setVelocity(gamepad1.left_trigger*maxVel);
+        long currentTime = System.currentTimeMillis();
 
+        shooter1.setVelocity(shooting*velocity);
+        shooter2.setVelocity(shooting*velocity);
 
-        telemetry.addData("Velocity",shooter2.getVelocity());
+        if(gamepad1.cross && currentTime - lastChangeTime > 2 * cooldown){
+            shooting = shooting == 0 ? 1 : 0;
+            lastChangeTime = currentTime;
+        }
+
+        if(gamepad1.dpad_up && currentTime - lastChangeTime > cooldown) {
+            velocity = maxVel - velocity > 0 ? velocity + 50 : maxVel;
+            lastChangeTime = currentTime;
+        }
+
+        if(gamepad1.dpad_down && currentTime - lastChangeTime > cooldown){
+            velocity = velocity > 0 ? velocity - 50 : 0;
+            lastChangeTime = currentTime;
+        }
+
+        telemetry.addData("Velocity1", shooter1.getVelocity());
+        telemetry.addData("Velocity2", shooter2.getVelocity());
         telemetry.update();
     }
 
