1415eb4d94f022f702cbbceba73134062032a2ab
ivarela-mqr <ivarela@mqrobots.org>
2026-02-07 13:07:41 +0100

    shooter

--- FtcRobotController-ITALY2026/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ FtcRobotController-ITALY2026/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,17 +1,47 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Shooter {
     DcMotorEx shooter0, shooter1;
-    Servo rotorL, rotorR, cover;
+    Servo rotorL, rotorR, coverL, coverR, block;
 
     LimeLight limeLight;
+
+
+    // PID constants rotors
+    double kP_rotor = 0.02;
+    double kI_rotor = 0.0;
+    double kD_rotor = 0.002;
+
+    // PID state rotors
+    double integralSum_rotor = 0;
+    double lastError_rotor = 0;
+    long lastTime_rotor = 0;
+
+    // Servo state rotors
+    double rotorPos = 0.5;
+
+
+    //PIDF shooter
+    double highVelocityShooter = 1500;
+    double lowVelocityShooter = 900;
+
+    double curTargetVelocity = highVelocityShooter;
+
+    double kP_shooter = 0;
+    double kI_shooter = 0;
+    double kD_shooter = 0;
+    double kF_shooter = 0;
+
+
 
 
     Shooter (HardwareMap hardwareMap, Constants.Alliance alliance){
@@ -19,20 +49,100 @@ public class Shooter {
         shooter1 = hardwareMap.get(DcMotorEx.class,"shooter1");
         rotorL = hardwareMap.get(Servo.class,"rotorL");
         rotorR = hardwareMap.get(Servo.class,"rotorR");
-        cover = hardwareMap.get(Servo.class,"cover");
+        coverL = hardwareMap.get(Servo.class,"coverL");
+        coverR = hardwareMap.get(Servo.class,"coverR");
+        block = hardwareMap.get(Servo.class, "block");
+
+        shooter0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        PIDFCoefficients pidfCoefficients_shooter = new PIDFCoefficients(kP_shooter, kI_shooter, kD_shooter, kF_shooter);
+
+        shooter0.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients_shooter);
+        shooter1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients_shooter);
+
+        coverL.setDirection(Servo.Direction.REVERSE);
 
         limeLight = new LimeLight(hardwareMap, alliance);
 
     }
 
-    public void aimShooterWithLimeLight(Telemetry telemetry,double yawAngle){
-        double shooterOffset = limeLight.getGoalAprilTagData(telemetry, yawAngle)[0];
+    private double pidCalculate(double error) {
+        long currentTime = System.nanoTime();
+        double dt = (currentTime - lastTime_rotor) / 1e9;
+
+        if (lastTime_rotor == 0) {
+            lastTime_rotor = currentTime;
+            return 0;
+        }
+
+        integralSum_rotor += error * dt;
+        double derivative = (error - lastError_rotor) / dt;
+
+        lastError_rotor = error;
+        lastTime_rotor = currentTime;
+
+        return (kP_rotor * error) + (kI_rotor * integralSum_rotor) + (kD_rotor * derivative);
+    }
+    public void resetPID() {
+        integralSum_rotor = 0;
+        lastError_rotor = 0;
+        lastTime_rotor = 0;
+    }
+
+    public void aimShooterWithLimeLight(Telemetry telemetry,double angle){
+
+
+        // If AprilTag not detected don't move
+        if (Math.abs(angle) < 0.05) return;
+
+        double correction = pidCalculate(angle);
+
+        rotorPos -= correction;
+
+        // Limit servo position
+        rotorPos = Math.max(0.0, Math.min(1.0, rotorPos));
+
+        rotorL.setPosition(rotorPos);
+        rotorR.setPosition(rotorPos);
+
+        telemetry.addLine("AimShooterWithLimeLight:");
+        telemetry.addData("Tx", angle);
+        telemetry.addData("PID Correction", correction);
+        telemetry.addData("Rotor Pos", rotorPos);
+    }
+
+    public void aimShooterWithOdometry(){
 
     }
 
-    public void aimShooterWithOdometry(){}
-    public void TeleOp(Gamepad gamepad, Telemetry telemetry){
+    public void adjustCover(Telemetry telemetry, double dist){
+        double pos = 0;
+        coverL.setPosition(pos);
+        coverR.setPosition(pos);
+    }
+
+    public void correctCover(Telemetry telemetry, double direction){
+        coverL.setPosition(coverL.getPosition()+(direction*0.05));
+        coverR.setPosition(coverR.getPosition()+(direction*0.05));
+    }
+
+    public void setShootingPower(Telemetry telemetry){
 
     }
+    public void TeleOp(Gamepad gamepad, Telemetry telemetry, double yawAngle){
+        double[] tx = limeLight.getGoalAprilTagData(telemetry, yawAngle);
+        aimShooterWithLimeLight(telemetry, tx[0]);
+        adjustCover(telemetry, tx[1]);
+
+        if(gamepad.left_bumper){
+            correctCover(telemetry, -1);
+        }else if(gamepad.right_bumper){
+            correctCover(telemetry, 1);
+        }
+        telemetry.addData("cover position: ", coverL.getPosition());
+    }
+
+
+
 
 }
