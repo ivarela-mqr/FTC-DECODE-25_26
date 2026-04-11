64004776a7e0f6a0f91dd5f3b7a9b2375af04352
iria-mqr <iria@mqrobots.org>
2026-04-11 10:18:05 +0200

    new project

---
+++ FtcRobotController- 11.1-testsspeed/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Italy26TeleOpMode.java
@@ -0,0 +1,142 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.util.Timer;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeStateMachine;
+import org.firstinspires.ftc.teamcode.subsystems.Tilt;
+import org.firstinspires.ftc.teamcode.util.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.util.PoseStorage;
+
+@TeleOp
+public class Italy26TeleOpMode extends OpMode {
+    DriveTrain driveTrain;
+    Shooter shooter;
+    IntakeStateMachine intakeStateMachine;
+    Tilt tilt;
+    IMU imu;
+    YawPitchRollAngles orientation;
+    double headingReset;
+    double yawOffset = 0;
+    double yawOffsetShooter = 0;
+    Follower follower;
+    Timer actualTimer,initTimer;
+    boolean rumble = false;
+    Constants.Alliance alliance;
+
+    @Override
+    public void init() {
+        alliance = PoseStorage.alliance;
+
+        actualTimer = new Timer();
+        initTimer = new Timer();
+        driveTrain = new DriveTrain(hardwareMap);
+        shooter = new Shooter(hardwareMap, alliance, 1200);
+        tilt = new Tilt(hardwareMap);
+        intakeStateMachine = new IntakeStateMachine(hardwareMap);
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP
+        ));
+        imu.initialize(parameters);
+        follower = createFollower(hardwareMap);
+        follower.startTeleOpDrive(true);
+        follower.setStartingPose(PoseStorage.currentPose);
+        if (PoseStorage.currentPose != null) {
+            follower.setPose(PoseStorage.currentPose);
+        }
+
+        if (alliance == Constants.Alliance.BLUE){
+            headingReset = 180;
+        }else{
+            headingReset = 0;
+        }
+    }
+
+    @Override
+    public void start(){
+        gamepad1.rumble(20);
+        gamepad2.rumble(20);
+        tilt.resetTimer();
+        initTimer.resetTimer();
+        actualTimer.resetTimer();
+        shooter.startTeleop();
+    }
+
+    @Override
+    public void loop() {
+        orientation = imu.getRobotYawPitchRollAngles();
+        double yawAngleShooter = orientation.getYaw(AngleUnit.DEGREES)
+                                    - yawOffsetShooter;
+
+        double rawYaw = Math.toDegrees(follower.getPose().getHeading());
+        double yawAngle = rawYaw - yawOffset + headingReset;
+
+        if (gamepad1.options) {
+            yawOffset = rawYaw;
+            follower.setPose(new Pose(
+                    follower.getPose().getX(),
+                    follower.getPose().getY(),
+                    headingReset
+            ));
+            yawOffsetShooter = orientation.getYaw();
+        }
+        follower.update();
+
+        driveTrain.TeleOp(gamepad1,telemetry,yawAngle);
+        /*intakeStateMachine.TeleOp((shooter.canShoot(gamepad1) && gamepad1.right_trigger > 0.1),
+                                    gamepad1, gamepad2);
+        shooter.TeleOp(gamepad1, gamepad2, telemetry, yawAngleShooter,
+                                    intakeStateMachine.isFull());
+        tilt.Teleop(gamepad1);
+        */
+        actualTimer.resetTimer();
+        if(!rumble && timeElapsed() > 105){
+            rumble = true;
+            gamepad1.rumble(2000);
+            gamepad2.rumble(2000);
+        }
+
+        /*
+        //telemetry.addData("Sensor2", intakeStateMachine.intake.colorSensor.distanceSensor.getDistance(DistanceUnit.CM));
+        //telemetry.addData("Sensor2 bis", intakeStateMachine.intake.colorSensor.distanceSensor.getDistance(DistanceUnit.CM));
+        //telemetry.addData("Sensor3", intakeStateMachine.intake.distanceSensor1.getDistance(DistanceUnit.CM));
+        //telemetry.addData("Sensor1", intakeStateMachine.intake.distanceSensor2.getDistance(DistanceUnit.CM));
+        //telemetry.addData("Intake state", intakeStateMachine.state);
+        //telemetry.addData("Pos encoder", shooter.encoder.getCurrentPosition());
+        //telemetry.addData("Heading odometry", follower.getHeading());
+        //telemetry.addData("SHooter", gamepad1.right_trigger);
+        //telemetry.addData("Yaw imu", yawAngleShooter);
+        //telemetry.addData("Is reseting", shooter.reset);
+        //telemetry.addData("Offset", shooter.offset);
+        //telemetry.addData("Power", shooter.rotorR.getPower());
+        //telemetry.addData("Bumper", gamepad1.left_bumper);
+        */
+
+        telemetry.addData("alliance",alliance);
+        telemetry.addData("YawAngle", yawAngle);
+        telemetry.addData("YawOffset", yawOffset);
+        telemetry.addData("X", follower.getPose().getX());
+        telemetry.addData("Y", follower.getPose().getY());
+        telemetry.addData("Heading", follower.getPose().getHeading());
+
+
+        telemetry.update();
+    }
+    public int timeElapsed(){
+        return (int)Math.abs(actualTimer.getElapsedTimeSeconds() - initTimer.getElapsedTimeSeconds());
+    }
+}
